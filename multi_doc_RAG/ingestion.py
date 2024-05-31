8f62de0b95fadfc19f45374441604c6b6061f3af
rouzbeh.farahmand <rouzbeh.farahmand@redis.com>
2024-05-31 12:54:32 -0400

    Applying feedback: adding document types of earning calls to the index.

--- multi_doc_RAG/ingestion.py
+++ multi_doc_RAG/ingestion.py
@@ -8,7 +8,6 @@ from langchain.document_loaders import UnstructuredFileLoader
 from unstructured.partition.pdf import partition_pdf
 import nltk
 
-
 warnings.filterwarnings("ignore")
 dir_path = os.getcwd()
 parent_directory = os.path.dirname(dir_path)
@@ -17,29 +16,38 @@ os.environ["ROOT_DIR"] = parent_directory
 print(dir_path)
 print(parent_directory)
 
-data_path = f"{parent_directory}/resources/filings"
-dirs = os.listdir(data_path)
+filings_data_path = f"{parent_directory}/resources/filings"
+earning_calls_data_path = f"{parent_directory}/resources/earning_calls"
+filings_dirs = os.listdir(filings_data_path)
+earning_calls_dirs = os.listdir(earning_calls_data_path)
 
 
 def get_sec_data():
     sec_data = {}
-    for dir in dirs:
-        dir_path = os.path.join(data_path, dir)
-        ticker = str(dir).split("/")[len(dir.split("/")) - 1]
-        sec_data[ticker] = {
-            "10K_files": [],
-            "metadata_file": [],
-            "transcript_files": [],
-        }
-        if not os.path.isdir(dir_path):
+    dirs = []
+    for filings_dir in filings_dirs:
+        dirs.append(os.path.join(filings_data_path, filings_dir))
+    for earning_calls_dir in earning_calls_dirs:
+        dirs.append(os.path.join(earning_calls_data_path, earning_calls_dir))
+
+    for path in dirs:
+        ticker = str(path).strip().split("/")[len(str(path).strip().split("/")) - 1]
+        if sec_data.get(ticker) is None:
+            sec_data[ticker] = {
+                "10K_files": [],
+                "metadata_file": [],
+                "transcript_files": [],
+            }
+
+        if not os.path.isdir(path):
             continue
-        for file in os.listdir(dir_path):
-            full_path = os.path.join(dir_path, file)
+        for file in os.listdir(path):
+            full_path = os.path.join(path, file)
             if '10K' in str(file):
                 sec_data[ticker]["10K_files"] = sec_data[ticker]["10K_files"] + [full_path]
             elif 'metadata' in str(file):
                 sec_data[ticker]["metadata_file"] = sec_data[ticker]["metadata_file"] + [full_path]
-            elif 'transcript' in str(file):
+            elif 'earning_calls' in str(full_path):
                 sec_data[ticker]["transcript_files"] = sec_data[ticker]["transcript_files"] + [full_path]
 
     print(f" ✅ Loaded doc info for  {len(sec_data.keys())} tickers...")
@@ -68,41 +76,66 @@ def load_json_metadata(path):
 
     return obj
 
-def redis_bulk_upload(data_dict, index, embeddings):
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2500, chunk_overlap=0)
-    for ticker in list(data_dict.keys()):
+def redis_bulk_upload(data_dict, index, embeddings, tickers=None):
+    def get_chunks(file_name, chunk_size=2500, chunk_overlap=0):
+        if not os.path.isfile(filing_file):
+            return []
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        try:
+            loader = UnstructuredFileLoader(
+                file_name, mode="single", strategy="fast"
+            )
+            chunks = loader.load_and_split(text_splitter)
+            return chunks
+        except Exception as e:
+            print(f"Error chunking {file_name} skipping")
+            return []
+
+    def add_embeddings_for_chunks(chunks, obj_to_load, doc_type):
+        chunk_objs_to_load = []
+        for i, chunk in enumerate(chunks):
+            content = str(chunk.page_content)
+            source_doc_full_path = str(chunk.metadata['source'])
+            source_doc = str(source_doc_full_path).split("/")[len(str(source_doc_full_path).split("/")) - 1]
+            obj_to_load['chunk_id'] = f"{source_doc}-{i}"
+            obj_to_load['source_doc'] = f"{source_doc}"
+            obj_to_load['content'] = content
+            obj_to_load['doc_type'] = doc_type
+            emb = embeddings.embed_query(content)
+            obj_to_load['text_embedding'] = np.array(emb).astype(np.float32).tobytes()
+            chunk_objs_to_load.append(obj_to_load)
+        return chunk_objs_to_load
+
+    total_chunks_count = 0
+    total_10K_count = 0
+    total_earning_calls_count = 0
+    if tickers is None:
+        tickers = list(data_dict.keys())
+
+    for ticker in tickers:
         if len(data_dict[ticker]["metadata_file"]) > 0:
             shared_metadata = load_json_metadata(data_dict[ticker]["metadata_file"][0])
 
         for filing_file in data_dict[ticker]["10K_files"]:
-
-            if not os.path.isfile(filing_file):
-                continue
-
             filing_file_filename = str(filing_file).split("/")[len(str(filing_file).split("/")) - 1]
-            chunks = []
-            try:
-                loader = UnstructuredFileLoader(
-                    filing_file, mode="single", strategy="fast"
-                )
-                chunks = loader.load_and_split(text_splitter)
-            except Exception as e:
-                print(f"Error chunking {filing_file} skipping")
-                continue
-            chunk_objs_to_load = []
-            for i, chunk in enumerate(chunks):
-                content = str(chunk.page_content)
-                source_doc_full_path = str(chunk.metadata['source'])
-                source_doc = str(source_doc_full_path).split("/")[len(str(source_doc_full_path).split("/")) - 1]
-                assert source_doc == filing_file_filename
-                obj_to_load = shared_metadata.copy()
-                obj_to_load['chunk_id'] = f"{filing_file_filename}-{i}"
-                obj_to_load['source_doc'] = f"{source_doc}"
-                obj_to_load['content'] = content
-                emb = embeddings.embed_query(content)
-                obj_to_load['text_embedding'] = np.array(emb).astype(np.float32).tobytes()
-                chunk_objs_to_load.append(obj_to_load)
-            keys = index.load(chunk_objs_to_load, id_field="chunk_id")
-            print(f"✅ Loaded {len(keys)} chunks for ticker={ticker} from {source_doc}")
+            chunks = get_chunks(filing_file)
+            doc_type = '10K'
+            filing_chunk_objs_to_load = add_embeddings_for_chunks(chunks, shared_metadata.copy(), doc_type=doc_type)
+            keys = index.load(filing_chunk_objs_to_load, id_field="chunk_id")
+            print(f"✅ Loaded {len(keys)} {doc_type} chunks for ticker={ticker} from {filing_file_filename}")
+            total_chunks_count = total_chunks_count + len(keys)
+            total_10K_count = total_10K_count + 1
 
+        for earning_file in data_dict[ticker]["transcript_files"]:
+            earning_file_filename = str(earning_file).split("/")[len(str(earning_file).split("/")) - 1]
+            chunks = get_chunks(earning_file)
+            doc_type = 'earning_call'
+            earning_chunk_objs_to_load = add_embeddings_for_chunks(chunks, shared_metadata.copy(), doc_type=doc_type)
+            keys = index.load(earning_chunk_objs_to_load, id_field="chunk_id")
+            print(f"✅ Loaded {len(keys)} {doc_type} chunks for ticker={ticker} from {earning_file_filename}")
+            total_chunks_count = total_chunks_count + len(keys)
+            total_earning_calls_count = total_earning_calls_count + 1
+
+    print(f"✅✅✅Loaded a total of {total_chunks_count} chunks from {total_10K_count} 10Ks and {total_earning_calls_count} earning calls for {len(tickers)} tickers.")
